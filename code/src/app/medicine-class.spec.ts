502f76cde4f62f5abd7a53b8cfda69a5307a6985
Suraj Kushvaha <71623164+surajkushvaha@users.noreply.github.com>
2021-06-30 14:56:49 +0530

    Delete code directory

--- code/src/app/medicine-class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MedicineClass } from './medicine-class';
-
-describe('MedicineClass', () => {
-  it('should create an instance', () => {
-    expect(new MedicineClass()).toBeTruthy();
-  });
-});
